f72aebf65e7f2a128a178edee2cf74606ccec03b
Danyaal Naseem Zar <d.n.zar@se21.qmul.ac.uk>
2023-10-03 20:45:14 +0100

    new project
    fresh one

---
+++ Platform_Underwater_Game/Assets/Scripts/playerController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public InputAction controls;
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
