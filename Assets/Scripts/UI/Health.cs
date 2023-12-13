f9bc18b8786ce9882031ebafab4fda13e49b0e43
Ayman <aymanakmal@outlook.com>
2023-12-13 15:28:44 +0000

    Health Points
    Added more to the health.cs script in which the UI now indicates your health and drops depending on the amount of damage that you take, e.g. if you get hit by the basic npc model i created you lose 20hp each time. Also made it so that the UI text now updates so that when your health goes to less than/equal to 0, lives decrement &reset and you respawn, putting your health points back to 100
    
    Co-Authored-By: umairYousaf803 <146939881+umairYousaf803@users.noreply.github.com>
    Co-Authored-By: ismail275 <146841997+ismail275@users.noreply.github.com>
    Co-Authored-By: Danyaal Zar <zardanyaal@gmail.com>

--- Assets/Scripts/UI/Health.cs
+++ Assets/Scripts/UI/Health.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro; // Add this for TextMeshPro support
 
 public class Health : MonoBehaviour
 {
     public float MaxHealth;
-    [SerializeField] private float CurrentHealth;
+    [SerializeField] public float CurrentHealth;
     public LivesText livesTextUpdater; // Reference to LivesText script
+    public TMP_Text healthTextUpdater; // Reference to the TextMeshPro UI element for health
     private bool isDead = false; // Flag to check if player is already dead
 
     // Start is called before the first frame update
     void Start()
     {
         CurrentHealth = MaxHealth;
+        UpdateHealthDisplay(); // Update health display at the start
     }
 
     // Player takes damage per hit amount
@@ -21,16 +24,27 @@ public class Health : MonoBehaviour
         if (!isDead)
         {
             CurrentHealth -= Amount;
+            UpdateHealthDisplay(); // Update health display whenever health changes
+
             if (CurrentHealth <= 0)
             {
                 Debug.Log("Player has died.");
                 livesTextUpdater.decrementLives(); // Decrement lives
                 isDead = true; // Set the flag to prevent multiple life decrements
                 CurrentHealth = MaxHealth;
-                transform.position = new Vector3(2.25f, 2.37f, 31.13f);
+                UpdateHealthDisplay(); // Update health display after resetting health
+                transform.position = new Vector3(2.25f, 2.37f, 31.13f); // Respawn position
                 isDead = false;
             }
         }
     }
 
+    // Method to update the health display
+    private void UpdateHealthDisplay()
+    {
+        if (healthTextUpdater != null)
+        {
+            healthTextUpdater.text = "Health: " + CurrentHealth.ToString();
+        }
+    }
 }
